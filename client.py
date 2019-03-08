1de9769dc8b0869761e63a25cf075c3c6d97e772
Evangeline Barrameda Manalac <ebma@equinor.com>
2019-03-08 14:30:18 +0100

    Added more info

--- client.py
+++ client.py
@@ -5,4 +5,13 @@ import githubapimock as api
 config_path = os.path.expanduser("~/repo.yml")
 settings = yaml.load(open(config_path))
 
-print(settings)
+org = settings["org"]
+repo = settings["repo"]
+user = settings["user"]
+token = settings["token"]
+
+title = "My first client issue"
+body = "This was created from the client application"
+issue_id = api.create_issue(org, repo, user, token, title, body)
+
+print("Created issue: " + str(issue_id) + " in " + org + "/" + repo)
